950282475465da638d438bcd4135857c20ea8147
Abhishek Das <abhishekdaszy@gmail.com>
2024-05-20 21:23:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link, Outlet, useNavigate } from 'react-router-dom';
+
+function Home() {
+  const navigate = useNavigate();
+
+  const goToAbout = () => {
+    navigate('/about');
+  };
+
+  return (
+    <div>
+      <h2>Home</h2>
+      <button onClick={goToAbout}>Go to About</button>
+    </div>
+  );
+}
+
+const Header = () => {
+  return(
+    <nav>
+    <Link to="/">Home</Link>
+    <Link to="about">About</Link>
+    <Link to="dashboard">Dashboard</Link>
+    <Link to="settings">Settings</Link>
+  </nav>
+  )
+}
+function About() {
+  return <h2>About</h2>;
+}
+
+function Dashboard() {
+  return <h3>Dashboard</h3>;
+}
+
+function Settings() {
+  return <h3>Settings</h3>;
+}
+
+function Error() {
+  return <h3>Error Page</h3>;
+}
+
+function Layout() {
+  return (
+    <div>
+      <Header/>
+      <hr />
+      <Outlet />
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="dashboard" element={<Dashboard />}>
+            <Route path="settings" element={<Settings />} />
+          </Route>
+          <Route path='*' element={<Error/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
