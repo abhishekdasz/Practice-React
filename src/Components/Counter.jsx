0fa3d00e9431fb9c3d3cdd5bdb80b1a34deea438
Abhishek Das <abhishekdaszy@gmail.com>
2024-05-20 23:51:41 +0530

    updated

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 const Counter = () => {
     let count = 0;
+
+    const increamentFunc = () =>{
+        count += 1;
+        console.log(count);
+    }
   return (
     <div>
       <p> Counter Example </p>
+      <p> Count: {count} </p>
+      <button onClick={increamentFunc}> Increase </button>
     </div>
   )
 }
